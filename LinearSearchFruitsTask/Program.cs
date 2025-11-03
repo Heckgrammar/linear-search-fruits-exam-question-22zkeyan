8f889ab770ff530c92fab28103c157592b178fe6
22zkeyan <22zkeyan@heckgrammar.co.uk>
2025-11-03 16:23:53 +0000

    finished assignment
    Implemented a linear search algorithm to find a word in a list of fruits and output the result.

--- LinearSearchFruitsTask/Program.cs
+++ LinearSearchFruitsTask/Program.cs
@@ -2,17 +2,33 @@
 {
     internal class Program
     {
+        static bool Linear_search(string[] list_data, string search_key) //initialises the linear search algorithm with two parameters
+        {
+            int index = 0; //sets index to 0 (start of algorithm)
+            while (index < list_data.Length && list_data[index] != search_key) //checks if the index hasn't exceeded the length of the array and the search key hasn't been found (if the element at the index is less than the search key)
+            {
+                index++; //changes index by 1
+            }
+            if (index >= list_data.Length || list_data[index] != search_key) //checks if the index has exceeded the length of the array and the search key hasn't been found
+            {
+                return false;
+            }
+            return true;
+        }
         static void Main(string[] args)
         {
             string[] fruits = { "banana", "apple", "orange", "pear", "grape", "pineapple" };
-
-            // Your program should:
-            // Ask the user what word they would like to find
-            // Output the message True if the word is found
-            // Output the message False if the word is not found
-            
-            // You must write your own linear search routine and not use any built-in fuctions available in C#
-            // You should use meaningful variable names
+            Console.Write("Enter word to find: ");
+            string word = Console.ReadLine()!;
+            bool in_list = Linear_search(fruits, word);
+            if (in_list)
+            {
+                Console.WriteLine($"{word} is in the list");
+            }
+            else
+            {
+                Console.WriteLine($"{word} isn't in the list");
+            }
         }
     }
 }
